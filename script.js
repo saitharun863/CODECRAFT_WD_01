7854ccd34f3defec10d5ab5d03692fdcd35dcdde
Sai Tharun Gurram <142737213+saitharun863@users.noreply.github.com>
2025-01-30 12:52:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change Navbar Background on Scroll
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
